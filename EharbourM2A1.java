fcf51f45fe4f8c63364769247f804eb1a7f84026
Eric Harbour <46880490+Eric-Harbour@users.noreply.github.com>
2023-08-23 19:07:01 -0500

    Module 2 Assignment 1
    Asked to take the given program (out of order) and arrange it into a working program. 
    Originally misread the assignment, so created my own class name...

---
+++ EharbourM2A1.java
@@ -0,0 +1,18 @@
+/* Original problem
+System.out.println("CSC 170");
+
+public class CSC170 {
+}
+
+public static void main(String[] args) {
+}
+
+*/
+
+/** Solution */
+public class EharbourM2A1 {
+    public static void main(String[] args) {
+        // Prints out CSC 170 to console
+        System.out.println("CSC 170");
+    }
+}
