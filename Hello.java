c930368855868a1dcd9c55195729db2867145458
Eric Harbour <46880490+Eric-Harbour@users.noreply.github.com>
2023-08-23 19:08:28 -0500

    Module 2 Assignment 2
    Asked to create a program with the following parameters:
    
    1) Write a complete program whose class name is Hello
    2) Displays Hello on the screen
    3) Displays "My name is" followed your first name and last name to the screen on a separate line.

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Eric
+ */
+public class Hello {
+    public static void main(String[] args) {
+        // Prints Hello to console
+        System.out.println("Hello");
+        
+        // Prints My name is Eric Harbour but does not skip a line
+        System.out.print("My name is ");
+        System.out.println("Eric Harbour");
+    }
+}
